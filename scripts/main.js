1bb851711d23dbfc47d57bcd5fa32dd6a0d15b75
Roland Nemec <113459241+DatRolxD@users.noreply.github.com>
2022-11-22 15:30:01 +0100

    Edit main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,52 +2,52 @@
 // create an array of options
 // generate a random number
 // pick an option out of the array = the word at random index
+
+//Ask user for an input - choose either a paper, a rock or a scissors
 let playerSelection = prompt("Please enter a value between rock, paper or scissors").toLowerCase();
+//Check whether the user's input is only one of the allowed values, otherwise print error
 if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'){
 
+  //Make a computer pick 
   function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * choices.length); 
     
-    return choices[randomIndex]
+    return choices[randomIndex];
   }
 
   let computerChoice = getComputerChoice();
 
-  switch(playerSelection, computerChoice){
-    case playerSelection === computerChoice:
-      console.log(`It's a tie`);
-      break;
-    case playerSelection === 'rock':
-      switch(computerChoice){
-        case 'paper':
-          console.log(`LOSE`);
-          break;
-        case 'scissors':
-          console.log(`WIN`);
-      }
-      break;
-    case playerSelection === 'scissors':
-      switch(computerChoice){
-        case 'rock':
-          console.log(`LOSE`);
-          break;
-        case 'paper':
-          console.log(`WIN`);
-      };
-      break;
-      
-      case playerSelection === 'paper':
-      switch(computerChoice){
-        case 'scissors':
-          console.log(`LOSE`);
-          break;
-        case 'rock':
-          console.log(`WIN`);
-      };
-      break;
+  console.log(`${playerSelection} ${computerChoice}`);
+  if (playerSelection === computerChoice){
+    console.log(`Tie`);
   }
+
+  else if (playerSelection === 'rock' && computerChoice === 'paper'){
+    console.log(`Lose`);
+  }
+
+  else if (playerSelection === 'rock' && computerChoice === 'scissors'){
+    console.log(`Win`);
+  }
+
+  else if (playerSelection === 'paper' && computerChoice === 'rock'){
+    console.log(`Win`);
+  }
+
+  else if (playerSelection === 'paper' && computerChoice === 'scissors'){
+    console.log(`Lose`);
+  }
+
+  else if (playerSelection === 'scissors' && computerChoice === 'paper'){
+    console.log(`Win`);
+  }
+
+  else if (playerSelection === 'scissors' && computerChoice === 'rock'){
+    console.log(`Lose`);
+  }
+  
 }
 else {
-  console.log(`The value you've entered is not allowed`);
+  console.error(`The value you've entered is not allowed`);
 }
